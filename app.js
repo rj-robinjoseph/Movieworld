62a237e2238606085bdbe951d44a80963dd52078
rj-robinjoseph <robinjoseph452@gmail.com>
2022-03-29 14:50:19 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,9 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+
+arrows.forEach((arrow,i)=>{
+    arrow.addEventListener("click",()=>{
+        movieLists[i].style.transform = "translateX(-100px)";
+    })
+})
