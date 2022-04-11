d4a9e833bb859a6b7197fa11b362b06cb1b29440
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-11 14:01:22 +0530

    Smooth Scroll Added

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
-
-arrows.forEach((arrow,i)=>{
-    const itemNumber = movieLists[i].querySelectorAll("img").length;
-    let clickCounter = 0;
-    arrow.addEventListener("click",()=>{
-        const ratio = window.innerWidth/270; 
-        clickCounter++;
-        if (itemNumber - (5 + clickCounter) + (6 - ratio) >= 0){
-            movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get("transform")[0].x.value-290}px)`;
-        }else{
-            movieLists[i].style.transform = "translateX(0)";
-            clickCounter = 0;
-        }
-        
-    });
-    console.log(Math.floor(window.innerWidth/270))
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = window.innerWidth / 270;
+    clickCounter++;
+    if (itemNumber - (5 + clickCounter) + (6 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 290
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+  console.log(Math.floor(window.innerWidth / 270));
 });
-
-
-
 
 // Toggle
 
 const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.nav-link,.sidebar,.left-menu-icon,.toggle,.bar");
-ball.addEventListener("click",()=>{
-    items.forEach(item=>{
-        item.classList.toggle("active")
-    })
-    ball.classList.toggle("active")
-})
-
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.nav-link,.sidebar,.left-menu-icon,.toggle,.bar"
+);
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
 
 // Hamburger
 
 const hamburger = document.querySelector(".hamburger");
 const menuList = document.querySelector(".menu-list");
 
-hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active");
-    menuList.classList.toggle("active");
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  menuList.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click",()=>{
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     menuList.classList.remove("active");
-}))
+  })
+);
+
+//------------------------------------------------------>
+const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+if (!isMobile) {
+  luxy.init({
+    wrapper: "#smooth-scroll",
+    wrapperSpeed: 0.098,
+  });
+}
